bc62cd419f6bbdcd6497bb229ebbc8ac9724b98b
ratcliffetj <ratcliffetj89@gmail.com>
2020-01-16 10:26:00 +0000

    Updated setup_test_database method to autogenerate ID

--- spec/setup_test_database.rb
+++ spec/setup_test_database.rb
@@ -3,8 +3,8 @@ require 'pg'
 def setup_test_database  # like a webhelper  
   connection = PG.connect(dbname: 'bookmark_manager_test')
   connection.exec("TRUNCATE TABLE bookmarks") # clear/destroy /wipe
-  connection.exec("INSERT INTO bookmarks VALUES(1, 'https://www.bbc.co.uk/news')")
-  connection.exec("INSERT INTO bookmarks VALUES(2, 'https://www.youtube.com')")
-  connection.exec("INSERT INTO bookmarks VALUES(3, 'http://www.destroyallsoftware.com')")
+  connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.bbc.co.uk/news')")
+  connection.exec("INSERT INTO bookmarks (url) VALUES('https://www.youtube.com')")
+  connection.exec("INSERT INTO bookmarks (url) VALUES('http://www.destroyallsoftware.com')")
 
 end 
